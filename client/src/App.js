0f2071a25d03d279a76892c88889ac075b7eb886
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2021-08-02 10:04:43 +0200

    !

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.jpg';
+import Posts from './commponents/Posts/Posts.js';
+import Form from './commponents/Form/Form.js';
 
 const App = () => {
     return (
@@ -9,6 +11,18 @@ const App = () => {
                 <Typography variant ='h2' align ='center'>Memories</Typography>
                 <img src={memories} alt='memories' height='60'></img>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }

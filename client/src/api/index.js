471424b0582c5ed21f40de4b9b6dc4e101943dff
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2021-08-03 13:43:26 +0200

    hum

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,5 @@ import axios from 'axios';
 
 const url = 'https://localhost:5000/posts';
 
-const fetchPosts = () => axios.get(url);
+export const fetchPosts = () => axios.get(url);
+export const createPosts = (newPost) => axios.post(url, newPost);
--- client/src/commponents/Form/Form.js
+++ client/src/commponents/Form/Form.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import useStyles from './styles'
+import React, { useState } from 'react';
+import useStyles from './styles';
+import { TextField, Button, Typography, Paper} from '@material-ui/core';
 
 const Form = () => {
+    const [postData, setPostData] = useState({
+        creator: '', 
+        title: '',
+        message: '',
+        tags: '',
+        selectedFile: '',
+    });
     const classes = useStyles();
+    const handleSubmit = () => {}
     return (
-        <h1>FORM</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+                <Typography variant='h6'>Create a Memory</Typography>
+                <TextField 
+                name='creator' 
+                variant='outlined' 
+                label='Creator'
+                fullWidth
+                value={postData.creator}
+                onChange ={(e) => setPostData({...postData, creator: e.target.value})}
+                />
+            </form>
+
+        </Paper>
     );
 }
 

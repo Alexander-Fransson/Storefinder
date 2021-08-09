255e399f23dc7ae5d5f7984ef886d08e5a31e823
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2021-08-09 22:02:20 +0200

    uh

--- client/src/commponents/Form/Form.js
+++ client/src/commponents/Form/Form.js
@@ -24,6 +24,7 @@ const Form = ({ currentId, setCurrentId }) => {
         else{
             dispatch(createPost(postData));
         }
+        clear();
 
         dispatch(createPost(postData));
     }
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,12 +1,13 @@
-import { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
+
 
 export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages);
     } catch (error){
-        res.status(404).json({message: error.message});
+        res.status(404).json({message: error});
     }
 }
 
@@ -14,11 +15,11 @@ export const createPost = (req, res) => {
     const post = req.body;
     const newPost = new PostMessage(post);
     try {
-        await newPost.save();
+        newPost.save();
         
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(409).json({message: error.message});
+        res.status(409).json({message: error});
     }
 }
 
@@ -28,7 +29,17 @@ export const updatePost = async (req,res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
     
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id}, { new: true });
 
     res.json(updatedPost);
+}
+
+export const deletePost = async (req,res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post with id that is');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post faild to exist successfully' });
 }
